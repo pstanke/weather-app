41b378ce67ac53e66cb111ea622d327852250e3c
Pawel Stanke <pablostanke21@gmail.com>
2022-10-25 00:19:33 +0200

    Fix api key

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -5,31 +5,40 @@ import ErrorBox from '../ErrorBox/ErrorBox';
 import { useCallback, useState } from 'react';
 
 const WeatherBox = () => {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
   const [weatherData, setWeatherData] = useState('');
   const [pending, setPending] = useState(false);
   const [error, setError] = useState(false);
-  const handleCityChange = useCallback((cityName) => {
-    fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=48a92e89f49e09316342c10d50e6921b&units=metric`,
-      setPending(true)
-    ).then((res) => {
-      if (res.status === 200) {
-        setError(false);
-        return res.json().then((data) => {
-          const weatherData = {
-            city: data.name,
-            temp: data.main.temp,
-            icon: data.weather[0].icon,
-            description: data.weather[0].main,
-          };
-          setWeatherData(weatherData);
-          setPending(false);
-        });
-      } else {
-        setError(true);
-      }
-    });
-  }, []);
+
+  const handleCityChange = useCallback(
+    (cityName) => {
+      console.log('check');
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`,
+        setPending(true)
+      ).then((res) => {
+        if (res.status === 200) {
+          setError(false);
+
+          return res.json().then((data) => {
+            const weatherData = {
+              city: data.name,
+              temp: data.main.temp,
+              icon: data.weather[0].icon,
+              description: data.weather[0].main,
+            };
+
+            setWeatherData(weatherData);
+            setPending(false);
+          });
+        } else {
+          setError(true);
+        }
+      });
+    },
+    [API_KEY]
+  );
 
   return (
     <section>
